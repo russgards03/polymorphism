663637885d4cc85d9873ccad1fbd526db2aa3e9b
russgards03 <99627318+russgards03@users.noreply.github.com>
2022-11-16 15:35:05 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,35 @@
-public class Main{
-    public static void main(String args[]){
+public class Main {
+    public static void main(String[] args) {
 
+class Human {
+    public void action() {
+        System.out.println("Animal sounds");
+    }
+}
+
+class Run extends Human {
+    public void action() {
+        System.out.println("Human is running");
+    }
+}
+
+class Walk extends Human {
+    public void action() {
+        System.out.println("Human is walking");
+    }
+}
+
+class Swim extends Human {
+    public void action() {
+        System.out.println("Human is swimming");
+    }
+}
+        Run a = new Run();
+        Walk b = new Walk();
+        Swim c = new Swim();
+
+        a.action();
+        b.action();
+        c.action();
     }
 }
